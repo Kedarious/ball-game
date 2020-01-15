d63098cdd753981c685ef4738d1d8e0ef7d9e2e4
Kedarious <darious_petway@yahoo.com>
2020-01-14 18:30:21 -0600

    Add design file

---
+++ scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    public float cameraFollowDistance = 10;
+
+    // Update is called once per frame
+    void LateUpdate() {
+        transform.position = new Vector3(0, gameObject.transform.position.y, player.gameObject.transform.position.z - cameraFollowDistance);
+    }
+}
---
+++ scripts/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject[] triangleprefabs;
+    private Vector3 spawnObstaclesPosition;
+
+    // Update is called once per frame
+    void Update() {
+        float distanceTohorizon = Vector3.Distance(player.gameObject.transform.position, spawnObstaclesPosition);
+        if (distanceTohorizon < 120) {
+            SpawnTriangles();
+
+        }
+
+    }
+
+    void SpawnTriangles() {
+        spawnObstaclesPosition = new Vector3(0, 0, spawnObstaclesPosition.z + 20);
+        Instantiate(triangleprefabs[(Random.Range(0, triangleprefabs.Length))], spawnObstaclesPosition, Quaternion.identity);
+    }
+}
